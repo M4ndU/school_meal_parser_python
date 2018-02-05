58841a0f84f49fd1458d0e5d955de2d5e4d2cd5d
M4ndU <33446356+M4ndU@users.noreply.github.com>
2018-02-05 18:16:13 +0900

    Update parser.py

--- parser.py
+++ parser.py
@@ -14,8 +14,8 @@ def get_html(url):
 def get_diet(code, ymd, weekday):
     schMmealScCode = code #int 1조식2중식3석식
     schYmd = ymd #str 요청할 날짜 ex:"2017.11.17"
-    if weekday == 6:
-        element = " " # 일요일은 파싱하지 않고 바로 리턴 (일요일 오류 문제로 인함)
+    if weekday == 6: #일요일 버림
+        element = " "
     else:
         num = weekday + 1 #int 요청할 날짜의 요일 0월1화2수3목4금5토6일 파싱한 데이터의 배열이 일요일부터 시작되므로 1을 더해줍니다.
         URL = (
@@ -23,25 +23,28 @@ def get_diet(code, ymd, weekday):
                 "schulCode=A000000000&schulCrseScCode=4&schulKndScCode=04"
                 "&schMmealScCode=%d&schYmd=%s" % (schMmealScCode, schYmd)
             )
-       #http://stu.AAA.go.kr/ 관할 교육청 주소 확인해주세요.
-       #schulCode= 학교고유코드
-       #schulCrseScCode= 1유치원2초등학교3중학교4고등학교
-       #schulKndScCode= 01유치원02초등학교03중학교04고등학교
-       #으로 변경해 주시면 됩니다.
+        #http://stu.AAA.go.kr/ 관할 교육청 주소 확인해주세요.
+        #schulCode= 학교고유코드
+        #schulCrseScCode= 1유치원2초등학교3중학교4고등학교
+        #schulKndScCode= 01유치원02초등학교03중학교04고등학교
+        #으로 변경해 주시면 됩니다.
         html = get_html(URL)
         soup = BeautifulSoup(html, 'html.parser')
         element = soup.find_all("tr")
         element = element[2].find_all('td')
-        element = element[num] #num
-        element = str(element)
+        try:
+            element = element[num] #num
+            element = str(element)
 
-        element = element.replace('[', '')
-        element = element.replace(']', '')
-        element = element.replace('<br/>', '\n')
-        element = element.replace('<td class="textC last">', '')
-        element = element.replace('<td class="textC">', '')
-        element = element.replace('</td>', '')
-        element = element.replace('(h)', '')
-        element = element.replace('.', '')
-        element = re.sub(r"\d", "", element)
+            element = element.replace('[', '')
+            element = element.replace(']', '')
+            element = element.replace('<br/>', '\n')
+            element = element.replace('<td class="textC last">', '')
+            element = element.replace('<td class="textC">', '')
+            element = element.replace('</td>', '')
+            element = element.replace('(h)', '')
+            element = element.replace('.', '')
+            element = re.sub(r"\d", "", element)
+        except:
+            element = ""
     return element
