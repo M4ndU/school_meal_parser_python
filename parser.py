f50e1a26927fd61ee150c471c5a7b6b61fbe59c6
M4ndU <33446356+M4ndU@users.noreply.github.com>
2018-03-27 18:45:49 +0900

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,50 +1,49 @@
-import requests
+#2018.03.27
+
+import requests, re
 from bs4 import BeautifulSoup
-import re
-
-
-def get_html(url):
-   _html = ""
-   resp = requests.get(url)
-   if resp.status_code == 200:
-      _html = resp.text
-   return _html
-
 
 def get_diet(code, ymd, weekday):
     schMmealScCode = code #int 1조식2중식3석식
-    schYmd = ymd #str 요청할 날짜 ex:"2017.11.17"
+    schYmd = ymd #str 요청할 날짜 yyyy.mm.dd
     if weekday == 5 or weekday == 6: #토요일,일요일 버림
-        element = " "
+        element = " " #공백 반환
     else:
         num = weekday + 1 #int 요청할 날짜의 요일 0월1화2수3목4금5토6일 파싱한 데이터의 배열이 일요일부터 시작되므로 1을 더해줍니다.
         URL = (
-                "http://stu.use.go.kr/sts_sci_md01_001.do?"
-                "schulCode=A000000000&schulCrseScCode=4&schulKndScCode=04"
+                "http://stu.sen.go.kr/sts_sci_md01_001.do?"
+                "schulCode=B100000519"
+                "&schulCrseScCode=4"
+                "&schulKndScCode=04"
                 "&schMmealScCode=%d&schYmd=%s" % (schMmealScCode, schYmd)
             )
         #http://stu.AAA.go.kr/ 관할 교육청 주소 확인해주세요.
         #schulCode= 학교고유코드
         #schulCrseScCode= 1유치원2초등학교3중학교4고등학교
         #schulKndScCode= 01유치원02초등학교03중학교04고등학교
-        #으로 변경해 주시면 됩니다.
-        html = get_html(URL)
-        soup = BeautifulSoup(html, 'html.parser')
-        element = soup.find_all("tr")
-        element = element[2].find_all('td')
-        try:
-            element = element[num] #num
-            element = str(element)
 
-            element = element.replace('[', '')
-            element = element.replace(']', '')
+        #기존 get_html 함수부분을 옮겨왔습니다.
+        html = ""
+        resp = requests.get(URL)
+        if resp.status_code == 200 : #사이트가 정상적으로 응답할 경우
+            html = resp.text
+        soup = BeautifulSoup(html, 'html.parser')
+        element_data = soup.find_all("tr")
+        element_data = element_data[2].find_all('td')
+        element = "나이스 홈페이지에 문제가 있습니다. 나중에 다시 시도해주세요." #사이트에 문제가 있을 경우 출력
+        try:
+            element = str(element_data[num])
+
+            #filter
+            element_filter = ['[', ']', '<td class="textC last">', '<td class="textC">', '</td>', '&amp;', '(h)', '.']
+            for element_string in element_filter :
+                element = element.replace(element_string, '')
+            #줄 바꿈 처리
             element = element.replace('<br/>', '\n')
-            element = element.replace('<td class="textC last">', '')
-            element = element.replace('<td class="textC">', '')
-            element = element.replace('</td>', '')
-            element = element.replace('(h)', '')
-            element = element.replace('.', '')
+            #모든 공백 삭제
             element = re.sub(r"\d", "", element)
+
+        #급식이 없을 경우
         except:
-            element = " "
+            element = " " # 공백 반환
     return element
